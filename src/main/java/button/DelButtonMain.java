bff244024787db5a0cf6d853292f094b819573c6
jiashubing <jiashb@yonyou.com>
2020-11-01 16:20:40 +0800

    添加SQL

--- src/main/java/button/DelButtonMain.java
+++ src/main/java/button/DelButtonMain.java
@@ -16,6 +16,7 @@ public class DelButtonMain {
 
     // 在A、B两个环境分别执行下面SQL，分别填写到oldService.properties 、newService.properties
     // SELECT group_concat(service_code,"=",service_id) FROM workbench.wb_service WHERE service_id IN (select service_id from workbench.wb_service where application_id in (select application_id from workbench.wb_application where application_id in (select application_id from workbench.wb_label_application_relation where label_id in (select label_id from workbench.wb_label where label_type='domain' and label_name='贾树丙领域')))) GROUP BY service_code;
+    // SELECT group_concat(service_code,"=",service_id) FROM workbench.wb_service WHERE service_id IN (select service_id from workbench.wb_service where application_id in (select application_id from workbench.wb_application where application_id in (select application_id from workbench.wb_label_application_relation where label_id in (select label_id from workbench.wb_label where label_type='domain' and label_name in ('ERP重构-预算中心','ERP重构-BM单中心'))))) GROUP BY service_code;
     private static Map<String, String> oldMap = new HashMap<>();
     private static Map<String, String> newMap = new HashMap<>();
     private static final String oldFileName = "oldService.properties";
